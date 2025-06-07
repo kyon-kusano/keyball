330403924932a390193fe0378cf70ac204d1170f
kyon-kusano <k.kusano.8.19@gmail.com>
2025-06-07 19:03:43 +0900

    feat:Add an Auto Mouse Layer

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/keymap.c
@@ -76,3 +76,9 @@ void oledkit_render_info_user(void) {
     keyball_oled_render_layerinfo();
 }
 #endif
+
+#ifdef POINT_DEVICE_AUTO_MOUSE_ENABLE
+void pointing_device_init_user(void){
+  set_auto_mouse_enable(true);
+}
+#endif
