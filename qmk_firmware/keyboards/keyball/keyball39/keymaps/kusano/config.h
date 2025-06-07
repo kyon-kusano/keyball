0895eeed79c4cbbdd3dffaf3c61214299641c940
kyon-kusano <k.kusano.8.19@gmail.com>
2025-06-07 22:36:01 +0900

    feat:Add an Auto Mouse Layer

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/config.h
@@ -23,7 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効
-#define AUTO_MOUSE_DEFAULT_LAYER 5
+#define AUTO_MOUSE_DEFAULT_LAYER 2
 #define AUTO_MOUSE_TIME 500
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
