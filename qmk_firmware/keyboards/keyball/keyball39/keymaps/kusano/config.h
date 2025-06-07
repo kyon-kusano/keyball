486a6aab242e16eca2d0851d80f542b7d8856c63
kyon-kusano <k.kusano.8.19@gmail.com>
2025-06-07 21:24:45 +0900

    feat:Add an Auto Mouse Layer

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/config.h
@@ -25,4 +25,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効
+#define AUTO_MOUSE_DEFAULT_LAYER 2
 #define AUTO_MOUSE_TIME 500
--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/keymap.c
@@ -77,8 +77,8 @@ void oledkit_render_info_user(void) {
 }
 #endif
 
-#include "pointing_device.h"
-
+#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
 void pointing_device_init_user(void){
-  set_auto_mouse_layer(4);
+  set_auto_mouse_enable(true);
 }
+#endif
