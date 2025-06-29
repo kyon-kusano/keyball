238115d7e89f4f5ba5d1084a90ceeea44c355cfb
kyon-kusano <k.kusano.8.19@gmail.com>
2025-06-29 15:09:52 +0900

    Add language toggle

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kusano/keymap.c
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 
 enum custom_keycodes{
-  LANG_TOGGLE = SAFE_RANGE, // カスタムキー
-}
+  LNG_TOGGLE = SAFE_RANGE // カスタムキー
+};
 
 bool is_lang1 = true; // Lang1送信状態
 
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,LANG_TOGGLE),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
+    KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,LNG_TOGGLE),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
   ),
 
   [1] = LAYOUT_universal(
@@ -98,7 +98,7 @@ void pointing_device_init_user(void){
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record){
   switch(keycode){
-    case LANG_TOGGLE:
+    case LNG_TOGGLE:
       if(record->tab.count && record->event.pressed){
         // タップ時の処理（推した瞬間）
         if(is_lang1){
